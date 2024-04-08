2de8a43c35e267f651ce89ab3c2551869174b842
Stéphane Philippart <wildagsx@gmail.com>
2024-04-08 21:01:16 +0200

    fix: 🐛 typo

--- src/main/java/fr/wilda/quarkus/OllamaAIService.java
+++ src/main/java/fr/wilda/quarkus/OllamaAIService.java
@@ -11,6 +11,6 @@ public interface OllamaAIService {
   // Context message
   @SystemMessage("You are an AI assistant.")  
   // Prompt customisation
-  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant ans use emoji.")
+  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant and use emojis.")
   String askAQuestion(String question);
 }
