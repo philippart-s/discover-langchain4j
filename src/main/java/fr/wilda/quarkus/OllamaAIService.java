397b59f1e151dd6e810e4bb03bd936be488fd775
Stephane Philippart <wildagsx@gmail.com>
2024-04-27 11:09:04 +0200

    feat: ✨ add streaming mode

--- src/main/java/fr/wilda/quarkus/OllamaAIService.java
+++ src/main/java/fr/wilda/quarkus/OllamaAIService.java
@@ -3,6 +3,7 @@ package fr.wilda.quarkus;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.smallrye.mutiny.Multi;
 
 // AI service bean registration
 @RegisterAiService
@@ -11,6 +12,14 @@ public interface OllamaAIService {
   // Context message
   @SystemMessage("You are an AI assistant.")  
   // Prompt customisation
-  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant ans use emoji.")
+  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant and use emoji.")
   String askAQuestion(String question);
+
+  // Context message
+  @SystemMessage("You are an AI assistant.")  
+  // Prompt customisation
+  @UserMessage("Answer as best possible to the following question: {question}. The answer must be in a style of a virtual assistant and use emoji.")
+  // Multi use is enough to activate streaming mode
+  Multi<String> askAQuestionSteramingMode(String question);
+
 }
